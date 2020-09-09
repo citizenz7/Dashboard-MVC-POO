c5cef330966c4625f65c7a0d5eff59001d741b8c
citizenz7 <citizenz7@protonmail.com>
2020-09-09 11:40:24 +0200

    graph

--- app/Views/templates/includes/footer.php
+++ app/Views/templates/includes/footer.php
@@ -11,74 +11,57 @@
                  */
                 echo $content;
                 ?>
-                <hr>
 
-<canvas id="myChart1" height="75"></canvas>
-<script>
-var ctx = document.getElementById('myChart1').getContext('2d');
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: 'line',
+                <br>
 
-  // The data for our dataset
-  data: {
-      labels: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin'],
-      datasets: [{
-          label: 'Evolution des ventes sur le premier semestre 2020',
-          backgroundColor: 'rgb(255, 99, 132)',
-          borderColor: 'rgb(255, 99, 132)',
-          data: [8, 10, 7, 8, 17, 24]
-      }]
-  },
+                <div class="row">
+                  <div class="col-sm-8">
+                    <hr>
+                    <canvas id="myChart" height="100"></canvas>
+                    <script>
+                    var ctx = document.getElementById('myChart').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                      type: 'bar',
+                      data: {
+                        labels: ['bricolage', 'éléctroménager', 'multimedia', 'tv-hifi', 'voiture'],
+                        datasets: [{
+                          label: 'Prix moyen des produits en € par catégorie',
+                          data: [49, 120, 60, 89, 66],
+                          backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                          ],
+                          borderColor: [
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                          ],
+                          borderWidth: 1
+                        }]
+                      },
+                      options: {
+                        scales: {
+                          yAxes: [{
+                            ticks: {
+                              beginAtZero: true
+                            }
+                          }]
+                        }
+                      }
+                    });
+                    </script>
+                  </div>
+                  <div class="col-sm-4">
+                  </div>
 
-  // Configuration options go here
-  options: {}
-});
-</script>
-
-</script>
-                <hr>
-
-                <canvas id="myChart" height="75"></canvas>
-      <script>
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var myChart = new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: ['0-50 €', '50-100 €', '100-250 €', '250-500 €', '500-1000 €', '1000-2500 €'],
-        datasets: [{
-            label: 'Prix des produits',
-            data: [1, 2, 3, 1, 2, 1],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-      }
-      });
-      </script>
+                </div>
 
             </div>
         </div><!-- /.container -->
